f68a421927a1eccea1d24ec647228a63770cc83a
Aaron Boxer <boxerab@protonmail.com>
2023-03-20 17:34:09 -0400

    simplify output format generation

--- src/lib/codec/jp2/GrkCompress.cpp
+++ src/lib/codec/jp2/GrkCompress.cpp
@@ -847,11 +847,9 @@ int GrkCompress::parseCommandLine(int argc, char** argv, CompressInitParams* ini
 
 		if(outForArg.isSet())
 		{
-			char outformat[50];
-			char* of = (char*)outForArg.getValue().c_str();
-			sprintf(outformat, ".%s", of);
+			std::string outformat = std::string(".") +  outForArg.getValue();
 			inputFolder->set_out_format = true;
-			parameters->cod_format = grk_get_file_format(outformat);
+			parameters->cod_format = grk_get_file_format(outformat.c_str());
 			switch(parameters->cod_format)
 			{
 				case GRK_FMT_J2K:
